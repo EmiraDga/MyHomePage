dcf372ff5fd116903d4a410a17c363de6d3bb934
Emira <emiradga888@gmail.com>
2022-06-16 22:46:45 +0100

    starting with navbar in header

---
+++ home-page/src/header.js
@@ -0,0 +1,29 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+
+export default function Header(){
+
+return(
+
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Weather</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+)
+
+
+}
--- home-page/src/index.js
+++ home-page/src/index.js
@@ -1,3 +1,16 @@
 import React from 'react';
-import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import Header from './header';
+function App(){
+return (
 
+  <Header ></Header>
+)
+}
+
+
+
+ReactDOM.render(
+  <App/>, 
+  document.getElementById("root")
+);
